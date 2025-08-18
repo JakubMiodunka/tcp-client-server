2684790cb23aaf80531720186cc58ab40b9d2ac3
JakubMiodunka <miodunkajakub@gmail.com>
2025-08-18 16:02:05 +0200

    #4 Adding unit tests for main server socket wrapper.

---
+++ src/UnitTests/Server/ServerSocketTests.cs
@@ -0,0 +1,91 @@
+﻿using Common.Encryption;
+using Common.Protocols;
+using Moq;
+using Server;
+using System.Net;
+
+namespace UnitTests.Server;
+
+[Category("UnitTest")]
+[TestOf(typeof(ServerSocket))]
+[NonParallelizable]
+[Author("Jakub Miodunka")]
+public class ServerSocketTests
+{
+    #region Default values
+    private const int DefaultPort = 8888;
+    private const int DefaultReceivingBufferSize = 1024;
+
+    private IPEndPoint _defaultIpEndPoint;
+    #endregion
+
+    #region Test setup
+    [SetUp]
+    public void SetUp()
+    {
+        _defaultIpEndPoint = new IPEndPoint(IPAddress.Loopback, DefaultPort);
+    }
+    #endregion
+
+    #region Test cases
+    [Test]
+    public void InstantiationImpossibleUsingNullReferenceAsListeningEndPoint()
+    {
+        var protocolStub = new Mock<IProtocol>();
+        var cipherStub = new Mock<ICipher>();
+
+        TestDelegate actionUnderTest =
+            () => { using var serverTcpSocket = new ServerSocket(null!, DefaultReceivingBufferSize, protocolStub.Object, cipherStub.Object); };
+
+        Assert.Throws<ArgumentNullException>(actionUnderTest);
+    }
+
+    [Test]
+    public void InstantiationImpossibleUsingNullReferenceAsProtocol()
+    {
+        var cipherStub = new Mock<ICipher>();
+
+        TestDelegate actionUnderTest =
+            () => { using var serverTcpSocket = new ServerSocket(_defaultIpEndPoint, DefaultReceivingBufferSize, null!, cipherStub.Object); };
+
+        Assert.Throws<ArgumentNullException>(actionUnderTest);
+    }
+
+    [Test]
+    public void InstantiationImpossibleUsingNullReferenceAsCipher()
+    {
+        var protocolStub = new Mock<IProtocol>();
+
+        TestDelegate actionUnderTest =
+            () => { using var serverTcpSocket = new ServerSocket(_defaultIpEndPoint, DefaultReceivingBufferSize, protocolStub.Object, null!); };
+
+        Assert.Throws<ArgumentNullException>(actionUnderTest);
+    }
+
+    [Test]
+    public void InstantiationImpossibleUsingInvalidReceivingBufferSize(
+        [Values(0)] int invalidReceivingBufferSize)
+    {
+        var protocolStub = new Mock<IProtocol>();
+        var cipherStub = new Mock<ICipher>();
+
+        TestDelegate actionUnderTest =
+            () => { using var serverTcpSocket = new ServerSocket(_defaultIpEndPoint, invalidReceivingBufferSize, protocolStub.Object, cipherStub.Object); };
+
+        Assert.Throws<ArgumentOutOfRangeException>(actionUnderTest);
+    }
+
+    [Test]
+    public void InstantiationPossibleUsingValidReceivingBufferSize(
+        [Values(1, 2)] int validReceivingBufferSize)
+    {
+        var protocolStub = new Mock<IProtocol>();
+        var cipherStub = new Mock<ICipher>();
+
+        TestDelegate actionUnderTest =
+            () => { using var serverTcpSocket = new ServerSocket(_defaultIpEndPoint, validReceivingBufferSize, protocolStub.Object, cipherStub.Object); };
+
+        Assert.DoesNotThrow(actionUnderTest);
+    }
+    #endregion
+}
