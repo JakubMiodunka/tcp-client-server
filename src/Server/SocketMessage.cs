898be430ccecd8c441af3e152364de5b11e69f5d
JakubMiodunka <miodunkajakub@gmail.com>
2025-08-18 13:48:24 +0200

    #5 Implementing socket message as read-only data structure to increase performance.

--- src/Server/SocketMessage.cs
+++ src/Server/SocketMessage.cs
@@ -1,12 +1,38 @@
-﻿namespace Server;
+﻿using System.Collections.ObjectModel;
+
+namespace Server;
 
 /// <summary>
 /// Model of message, which is transfered via socket connection.
 /// </summary>
-/// <param name="ConnectionIdentifier">
-/// Unique identifier of message sender connection.
-/// </param>
-/// <param name="Content">
-/// Binary content of the message.
-/// </param>
-public sealed record SocketMessage(int ConnectionIdentifier, byte[] Content);
+/// <remarks>
+/// Previously this model was implemented as record type, but it was more efficient to implement it as a structure.
+/// Structures as a value types are allocated on stack instead of heap as records does, so access to them is quicker.
+/// In our case the model does not contain large amounts of data, so necessity of coping it every time,
+/// when instance is passed around does not cause performance issues.
+/// Small amount of data to copy and shorter access to memory makes this model more efficient as a structure.
+/// </remarks>
+public readonly struct SocketMessage
+{
+    #region Properties
+    public readonly int ConnectionIdentifier;
+    public readonly ReadOnlyCollection<byte> Content;
+    #endregion
+
+    #region Instantiation
+    /// <summary>
+    /// Creates a new message instance.
+    /// </summary>
+    /// <param name="connectionIdentifier">
+    /// Unique identifier of message sender connection.
+    /// </param>
+    /// <param name="content">
+    /// Binary content of the message.
+    /// </param>
+    public SocketMessage(int connectionIdentifier, IEnumerable<byte> content)
+    {
+        ConnectionIdentifier = connectionIdentifier;
+        Content = content.ToArray().AsReadOnly();
+    }
+    #endregion
+}
