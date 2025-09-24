731a2c5a9c72d87c7b575ae867fe99c66bbdd1a2
JakubMiodunka <miodunkajakub@gmail.com>
2025-09-24 20:35:52 +0200

    #10 Adding server demo project.

---
+++ src/ServerDemo/Program.cs
@@ -0,0 +1,41 @@
+﻿using Common.Encryption;
+using Common.Padding;
+using Common.Protocols;
+using Server;
+using System.Net;
+using System.Text;
+
+// Configuration:
+var listeningEndPoint = new IPEndPoint(IPAddress.Loopback, 8888);
+int receivingBufferSize = 1024;
+var protocol = new SimpleSessionLayerProtocol(4);
+var encryptionKey = new byte[16] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 };
+var bitPaddingProvider = new PkcsBitPaddingProvider(TeaCipher.DataBlockSize);
+var cipher = new TeaCipher(encryptionKey, bitPaddingProvider);
+
+// Main:
+Console.WriteLine("Starting the server...");
+
+using (var server = new ServerSocket(listeningEndPoint, receivingBufferSize, protocol, cipher))
+{
+    server.StartAcceptingConnections();
+    Console.WriteLine("Listening for connections...");
+
+    while (true)
+    {
+        SocketMessage? message = server.GetReceivedData();
+
+        if (message.HasValue)
+        {
+            int connectionIdentifier = message.Value.ConnectionIdentifier;
+            byte[] binaryContent = message.Value.Content.ToArray();
+
+            string textContent = Encoding.UTF8.GetString(binaryContent);
+            Console.WriteLine($"New message from connection {connectionIdentifier}: {textContent}");
+
+            server.SentData(connectionIdentifier, binaryContent);
+        }
+
+        Thread.Sleep(200);
+    }
+}
